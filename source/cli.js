e458ada88d24483dd1f07f4290b0e6ec3b04ff79
Adrian Sieber <mail@adriansieber.com>
2015-05-10 16:56:08 +0200

    Add cli flag to beautify svg output

--- source/cli.js
+++ source/cli.js
@@ -8,17 +8,34 @@ var fsp = require('fs-promise'),
 	svgKit = require('./svgKit'),
 
 	commandName = path.basename(process.argv[1]),
-	args = process.argv.slice(2)
+	args = process.argv.slice(2),
+	beautifyHtml = require('js-beautify').html,
+	flags = {}
+
+
+args.forEach(function (cliArgument) {
+	if (/^\-\-/i.test(cliArgument)) {
+		return flags[cliArgument.slice(2)] = true
+	}
+})
 
 
 if (args[0] === 'compile') {
 
 	svgScript
-		.getIcons(path.resolve(process.cwd(), args[1]))
+		.getIcons(path.resolve(process.cwd(), args.pop()))
 		.forEach(function (icon) {
 
+			var content = svgKit.formatSvg(icon.content)
+
+			if (flags.beautify)
+				content = beautifyHtml(content)
+
+			if (!/\n$/.test(content))
+				content += '\n'
+
 			fsp
-				.writeFile(icon.filePath, svgKit.formatSvg(icon.content))
+				.writeFile(icon.filePath,content)
 				.then(function () {
 					console.log(chalk.green('Created', icon.fileName))
 				})

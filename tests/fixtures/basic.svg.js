4695306360cbeb85ff98cf59b2af441b90710bd9
Adrian Sieber <mail@adriansieber.com>
2016-10-30 17:56:10 +0000

    Add tests

---
+++ tests/fixtures/basic.svg.js
@@ -0,0 +1,85 @@
+/* eslint-disable id-length */
+
+module.exports.shaven = () => {
+  const width = 214
+  const height = 214
+  const screwDiameter = 3
+  const renderScrews = false
+
+  return ['svg', {
+    width: width + 'mm',
+    height: height + 'mm',
+    viewBox: [0, 0, width, height].join()},
+
+    ['path', {
+      d: 'M7,0' +
+        'h200' +
+        'a 7 7 0 0 0 7 7' +
+        'v200' +
+        'a 7 7 0 0 0 -7 7' +
+        'h-56' +
+        'a 2 2 0 0 1 -2 -2' +
+        'v-8' +
+        'h-84' +
+        'v8' +
+        'a 2 2 0 0 1 -2 2' +
+        'h-56' +
+        'a 7 7 0 0 0 -7 -7' +
+        'v-200' +
+        'a 7 7 0 0 0 7 -7' +
+        'z',
+      style: {
+        strokeWidth: '0.001mm',
+        stroke: 'red',
+      },
+    }],
+
+    // Screw heads
+    ['g', renderScrews, {fill: 'green'},
+      ['circle', {
+        r: 2.6,
+        cx: 2.5,
+        cy: 2.5,
+      }],
+      ['circle', {
+        r: 2.6,
+        cx: 211.5,
+        cy: 2.5,
+      }],
+      ['circle', {
+        r: 2.6,
+        cx: 211.5,
+        cy: 211.5,
+      }],
+      ['circle', {
+        r: 2.6,
+        cx: 2.5,
+        cy: 211.5,
+      }],
+    ],
+
+    // Holes
+    ['g', renderScrews, {r: screwDiameter / 2},
+      ['circle', {
+        r: screwDiameter / 2,
+        cx: 2.5,
+        cy: 2.5,
+      }],
+      ['circle', {
+        r: screwDiameter / 2,
+        cx: 211.5,
+        cy: 2.5,
+      }],
+      ['circle', {
+        r: screwDiameter / 2,
+        cx: 211.5,
+        cy: 211.5,
+      }],
+      ['circle', {
+        r: screwDiameter / 2,
+        cx: 2.5,
+        cy: 211.5,
+      }],
+    ],
+  ]
+}
---
+++ tests/main.js
@@ -0,0 +1,17 @@
+const assert = require('assert')
+const commonTags = require('common-tags')
+const oneLine = commonTags.oneLine
+
+const createIcon = require('../source/createIcon')
+const iconModule = require('./fixtures/basic.svg.js')
+
+const expectedSvg = oneLine `<svg width="214mm" height="214mm"
+  viewBox="0,0,214,214"><path d="M7,0h200a 7 7 0 0 0 7 7v200a 7 7 0 0 0 -7
+  7h-56a 2 2 0 0 1 -2 -2v-8h-84v8a 2 2 0 0 1 -2 2h-56a 7 7 0 0 0 -7 -7v-200a
+  7 7 0 0 0 7 -7z" style="strokeWidth:0.001mm;stroke:red"></path></svg>`
+const actualSvg = createIcon('test', iconModule)
+
+assert.equal(
+  actualSvg,
+  expectedSvg
+)

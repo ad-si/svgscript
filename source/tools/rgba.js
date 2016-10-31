0b3f60aa21f3966fe9c658249626ec2e3144109b
Adrian Sieber <mail@adriansieber.com>
2016-10-31 22:35:57 +0000

    Simplify rgba stringifier

--- source/tools/rgba.js
+++ source/tools/rgba.js
@@ -1,25 +1,9 @@
 module.exports = (red, green, blue, alpha) => {
-  alpha = alpha || 1
-
   if (Array.isArray(red)) {
-    const values = red
-    if (values.length === 3) {
-      return 'rgb(' + values.join() + ')'
-    }
-    else if (values.length === 4) {
-      return 'rgba(' + values.join() + ')'
-    }
-    else {
-      throw new RangeError(
-        values.length + ' is an invalid number of Array elements!'
-      )
-    }
+    [red, green, blue, alpha] = red
   }
 
-  else if (alpha === undefined || alpha === 0) {
-    return 'rgb(' + [red, green, blue].join() + ')'
-  }
-  else {
-    return 'rgba(' + [red, green, blue, alpha].join() + ')'
-  }
+  return alpha
+    ? `rgba(${[red, green, blue, alpha]})`
+    : `rgb(${[red, green, blue]})`
 }

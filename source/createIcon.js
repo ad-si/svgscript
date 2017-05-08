19f67d0d9492b33d70c86df97eda9bf809ecb055
Adrian Sieber <mail@adriansieber.com>
2017-05-08 14:40:57 +0000

    Add tool to convert a style object to CSS

--- source/createIcon.js
+++ source/createIcon.js
@@ -12,45 +12,47 @@ module.exports = (name, iconModule, targetData) => {
     throw new Error('Module ' + name + ' was not passed to createIcon().')
   }
 
-  if (
-    !iconModule.targetVersion ||
+  const isSupportedVersion = !iconModule.targetVersion ||
     semver.lt(iconModule.targetVersion, '0.4.0')
-  ) {
-    if (iconModule.shaven) {
-      if (typeof iconModule.shaven !== 'function') {
-        throw new Error('shaven property must be a function')
-      }
-      content = iconModule.shaven(targetData, tools)
 
-      if (!Array.isArray(content)) {
-        throw new TypeError(name + '.shaven() must return an array!')
-      }
+  if (!isSupportedVersion) {
+    throw new Error(
+      'Target version is not supported anymore.' +
+      'Try a newer target verison but be aware of potential errors.'
+    )
+  }
 
-      content = createTransformationString(content)
-      content.push(coordinateSystemAxes)
-      content = shaven(content)[0]
+  if (iconModule.shaven) {
+    if (typeof iconModule.shaven !== 'function') {
+      throw new Error('shaven property must be a function')
     }
-    else if (iconModule.svg) {
-      content = iconModule.svg(targetData, tools)
+    content = iconModule.shaven(targetData, tools)
 
-      if (typeof content !== 'string') {
-        throw new TypeError(name + '.svg() must return a string!')
-      }
+    if (!Array.isArray(content)) {
+      throw new TypeError(name + '.shaven() must return an array!')
     }
-    else if (typeof iconModule === 'function') {
-      content = iconModule(targetData, tools)
 
-      if (typeof content !== 'string') {
-        throw new TypeError(iconModule + ' must return a string!')
-      }
+    content = createTransformationString(content)
+    content.push(coordinateSystemAxes)
+    content = shaven(content)[0]
+  }
+  else if (iconModule.svg) {
+    content = iconModule.svg(targetData, tools)
+
+    if (typeof content !== 'string') {
+      throw new TypeError(name + '.svg() must return a string!')
     }
-    else {
-      // eslint-disable-next-line no-console
-      console.error(
-        'Module ' + name + ' does not provide a suitable interface!'
-      )
-      return
+  }
+  else if (typeof iconModule === 'function') {
+    content = iconModule(targetData, tools)
+
+    if (typeof content !== 'string') {
+      throw new TypeError(iconModule + ' must return a string!')
     }
+  }
+  else {
+    console.error(`Module ${name} does not provide a suitable interface!`)
+    return
   }
 
   return content
--- source/tools/index.js
+++ source/tools/index.js
@@ -1,7 +1,10 @@
 const clone = require('clone')
-const rad2deg = require('rad2deg')
 const deg2rad = require('deg2rad')
+const rad2deg = require('rad2deg')
 const SvgPath = require('svgpath')
+const CleanCSS = require('clean-css')
+const cssobjCore = require('cssobj-core')
+const cssobjPluginGencss = require('cssobj-plugin-gencss')
 
 const rgba = require('./rgba.js')
 const formatSvg = require('./formatSvg')
@@ -10,30 +13,73 @@ const arrangeAsGrid = require('./arrangeAsGrid')
 const businessCard = require('./businessCard')
 
 
+function optimizePath (path) {
+  return new SvgPath(path)
+    .round(0)
+    .toString()
+}
+
+function optimizePathAbsolute (path) {
+  return new SvgPath(path)
+    .abs()
+    .round(0)
+    .toString()
+}
+
+function optimizePathRelative (path) {
+  return new SvgPath(path)
+    .abs()
+    .round(0)
+    .rel()
+    .round(0)
+    .toString()
+}
+
+function cssobjToCss (options = {}) {
+  const {
+    styleObject,
+    minified,
+    scoped,
+  } = options
+  let {
+    indentation = '  ',
+    endOfLine = '\n',
+  } = options
+
+  if (minified) {
+    indentation = ''
+    endOfLine = ''
+  }
+
+  const getInstance = cssobjCore({
+    plugins: [ // Order is important
+      cssobjPluginGencss({
+        indent: indentation,
+        newLine: endOfLine,
+      }),
+    ],
+  })
+  const css = getInstance(styleObject).css
+
+  return minified
+    ? new CleanCSS()
+        .minify(css)
+        .styles
+    : css
+}
+
 module.exports = {
-  rgb: rgba,
-  rgba,
-  clone,
-  degToRad: deg2rad,
-  radToDeg: rad2deg,
-  optimizePath: (path) =>
-    new SvgPath(path)
-      .round(0)
-      .toString(),
-  optimizePathAbsolute: (path) =>
-    new SvgPath(path)
-      .abs()
-      .round(0)
-      .toString(),
-  optimizePathRelative: (path) =>
-    new SvgPath(path)
-      .abs()
-      .round(0)
-      .rel()
-      .round(0)
-      .toString(),
-  circleSection,
-  formatSvg,
   arrangeAsGrid,
   businessCard,
+  circleSection,
+  clone,
+  degToRad: deg2rad,
+  formatSvg,
+  cssobjToCss,
+  optimizePath,
+  optimizePathAbsolute,
+  optimizePathRelative,
+  radToDeg: rad2deg,
+  rgb: rgba,
+  rgba,
 }
